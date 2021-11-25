6aead2ab0d2f632ce798188be9acda78cadd169a
bhavitha-muppavarapu <94819637+bhavitha-muppavarapu@users.noreply.github.com>
2021-11-25 12:40:58 +0530

    Update MethodOverloading1.java

--- src/MethodOverloading1.java
+++ src/MethodOverloading1.java
@@ -4,6 +4,7 @@ class MethodOverloading1{
     }
     private static void display(int a , int b){
         System.out.println("Arguments:" + a +  "And"  + b );
+        System.out.println("bhavitha");
     }
     public static void main(String args[]){
         display(1);
