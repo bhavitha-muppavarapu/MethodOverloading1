4f572335d7102b2b53827b4c1fe0b8d8ef762cd4
simplilearn_GitHub <bhavitha.muppavarapu@simplilearn.net>
2021-11-25 12:39:25 +0530

    Initial commit

---
+++ src/MethodOverloading1.java
@@ -0,0 +1,12 @@
+class MethodOverloading1{
+    private static void display(int a){
+        System.out.println("Arguments:" + a);
+    }
+    private static void display(int a , int b){
+        System.out.println("Arguments:" + a +  "And"  + b );
+    }
+    public static void main(String args[]){
+        display(1);
+        display(1 ,4);
+    }
+}
